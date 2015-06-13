32a9793c5440f837e92d0c2e981556c9d0877ebe
vicmonmena <vicmonmena@gmail.com>
2015-06-13 19:47:14 +0200

    Yii2: primer proyecto basic y autenticación de usuarios contra BBDD
    Yii2: primer proyecto basic y autenticación de usuarios contra BBDD

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;port=8889;dbname=yii2basic;unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
